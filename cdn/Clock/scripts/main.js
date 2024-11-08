68ef030ab0f7549e6f07632493ccb6fa3906c8e8
butlanys <gitpush@hqycloud.top>
2024-11-08 22:02:44 +0800

    Site updated: 2024-11-08 22:02:43

--- cdn/Clock/scripts/main.js
+++ cdn/Clock/scripts/main.js
@@ -1,1 +1,1 @@
-import{Clock,Settings,updateClock}from"./scripts.js";let clock=new Clock;let settings=new Settings;let updatedClock=updateClock(clock,settings);setInterval(function(){updatedClock=updateClock(clock,settings)},1e3);
+import{Clock,Settings,updateClock}from"./scripts.js";let clock=new Clock;let settings=new Settings;let updatedClock=updateClock(clock,settings);setInterval(function(){updatedClock=updateClock(clock,settings);clock=updatedClock},1e3);
