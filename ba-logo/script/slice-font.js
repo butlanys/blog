993043ce156f81d51e77baaf2579fbce12e53b7f
butlanys <gitpush@hqycloud.top>
2024-11-10 19:45:09 +0800

    Site updated: 2024-11-10 19:45:08

---
+++ ba-logo/script/slice-font.js
@@ -0,0 +1,1 @@
+import{resolve}from"path";import createFontSlice from"font-slice";createFontSlice({fontPath:resolve("../public/GlowSansSC-Normal-Heavy_diff.ttf"),outputDir:resolve("../public/GlowSans")});
